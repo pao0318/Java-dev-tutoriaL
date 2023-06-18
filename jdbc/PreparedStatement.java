312c0eff9c850095934c0458da5da0bef79dc8ab
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2023-06-18 16:31:55 +0530

    Update PreparedStatement.java
    Adds updated delete queries from query

--- jdbc/PreparedStatement.java
+++ jdbc/PreparedStatement.java
@@ -5,12 +5,26 @@ public class PreparedStatement
 	    try{
 	        Class.forName("oracle.jdbc.driver.OracleDriver");
 	        Connection con=DriverManager.getConnection("jdbc:orcale:thin:@localhost:1521:xe","dbname","pass");
-	        Statement stmt=con.PreparedStatement("insert into EMP values(?,?)");
+	        PreparedStatement stmt=con.PreparedStatement("insert into EMP values(?,?)");
 	        stmt.setInt(1,101);// first parameter
 	        stmt.setString(2,"Ratan");
 	        int i=stmt.executeUpdate();
 	        
-	        System.out.println(i+ "Records suubmitted");
+	        System.out.println(i+ "Records submitted");
+	        
+	        // Updates Records
+	        PreparedStatement st=con.PreparedStatement("update enm set name=? where id=?");
+	        st.setString(1,101);
+	        int j=executeUpdate();
+	        System.out.println(j+ "Records updated");
+	        
+	       // Deletes Records
+	        PreparedStatement st=con.PreparedStatement("delete from emp where id=?");
+	        st.setString(1,101);
+	        int j=executeUpdate();
+	        System.out.println(j+ "Records deleted");	 
+	        
+
 	        con.close();
 	        
 	    }catch(Exception e){ System.out.println(e);} 
